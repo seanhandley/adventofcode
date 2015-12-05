1beb2995bd714141df1d376cf37361a100bb426d
Sean Handley <sean.handley@gmail.com>
2015-12-05 12:12:18 +0000

    Day 5 solution.

---
+++ solutions/day_5/advent5.2.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+
+# Realizing the error of his ways, Santa has switched to a better model of determining whether a string is naughty or nice. None of the old rules apply, as they are all clearly ridiculous.
+
+# Now, a nice string is one with all of the following properties:
+
+# It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+# It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+# For example:
+
+# qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+# xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+# uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+# ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
+# How many strings are nice under these new rules?
+
+class String
+  def equal_chunks(size)
+    i = 0
+    chunks = []
+    (length-size+1).times do
+      chunks << slice(i, size)
+      i += 1 
+    end
+    chunks
+  end
+  def indexes(needle)
+    found = []
+    current_index = -1
+    while current_index = index(needle, current_index+1)
+      found << current_index
+    end
+    found
+  end
+end
+
+def one_index_pair_non_consecutive(indexes)
+  return true if indexes.count > 2
+  i = 0
+  (indexes.count-1).times do
+    return true if (indexes[i+1] - indexes[i]) > 1
+    i += 1
+  end
+  false
+end
+
+strings = STDIN.read.split("\n")
+
+nice = strings.select do |string|
+  string.equal_chunks(3).any? do |chunk|
+    chunk.chars[0] == chunk.chars[2]
+  end
+end
+
+nice = nice.select do |string|
+  string.equal_chunks(2).any? do |chunk|
+    indexes = string.indexes(chunk)
+    indexes.count >= 2 && one_index_pair_non_consecutive(indexes)
+  end
+end
+
+puts nice.count
