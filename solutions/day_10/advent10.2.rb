d900df3df00be6e999bac81a0fa1b85fea665d63
Sean Handley <sean.handley@gmail.com>
2015-12-10 13:23:58 +0000

    Day 10 solution.

---
+++ solutions/day_10/advent10.2.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# --- Day 10: Elves Look, Elves Say ---
+#
+# --- Part Two ---
+#
+# Neat, right? You might also enjoy hearing John Conway talking about this sequence (that's Conway of Conway's Game of Life fame).
+#
+# Now, starting again with the digits in your puzzle input, apply this process 50 times. What is the length of the new result?
+
+require_relative 'advent10.1'
+
+p look_and_say(50).length
